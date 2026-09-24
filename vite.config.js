40432794d8a0bf851f3e32d5594a56a9571baa7d
OMAR <ooomarssayed7777@gmail.com>
2026-09-24 08:59:12 +0300

    Deploy React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Task1-Single-Page-Application-SPA/',
+  base: '/Codveda-Single-Page-Application-SPA/',
 })
